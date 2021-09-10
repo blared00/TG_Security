8e07b865fcd2d81ba580c8f644da7fbdca882e7e
blared00 <yudintsev.vsevolod@gmail.com>
2021-09-10 06:14:48 +0300

    Добавил команды Добавил вызов опроса

--- message_filter.py
+++ message_filter.py
@@ -3,14 +3,37 @@ import random
 try:
     import config
 except ImportError:
-    config = {x: () for x in ('BAD_WORDS', 'HELLO_WORD')}
+    config = {
+        x: ()
+        for x in (
+            "BAD_WORDS",
+            "HELLO_WORD",
+            "POLL_QUESTION",
+            "POLL_OPTIONS",
+            "LINK_WIKI",
+            "COMMANDS",
+        )
+    }
 
 
 def start_message(message):
-    if message.text in config.HELLO_WORD:
-        return "{}, уважаемые. \nПовоюем?".format(random.choice(config.HELLO_WORD))
+    """Приветствие и обработка матов"""
+    if message.text.lower() in config.HELLO_WORD:
+        return "{}, уважаемые. \nПовоюем? Запускай опрос - /poll\nЕсли хочешь просветиться -/wiki".format(
+            random.choice(config.HELLO_WORD).title()
+        )
     if message.text in config.BAD_WORDS:
-        return "Ты матершинник, {}".format(message.chat.first_name)
+        return "Ты матершинник, {}".format(message["from"].first_name)
 
 
+def create_poll():
+    """Создание опроса"""
+    question = random.choice(config.POLL_QUESTION)
+    options = config.POLL_OPTIONS
+    is_anonimus = False
+    return (question, options, is_anonimus)
 
+
+def get_link_wiki(command):
+    """Возвращает страницу с википедии"""
+    return config.LINK_WIKI.format(config.COMMANDS[command[1:]])
--- server.py
+++ server.py
@@ -1,23 +1,44 @@
-import config
 import logging
 
+import config
 from aiogram import Bot, Dispatcher, executor, types
 
 import message_filter
 
 """Логирование"""
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 """Init"""
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
+
+@dp.message_handler(commands=["poll"])
+async def poll_start_game(message: types.Message):
+    """Запуск опроса"""
+    await message.answer_poll(*message_filter.create_poll())
+
+
+@dp.message_handler(
+    commands=["wiki"],
+)
+async def wiki(message: types.Message):
+    """Список команд"""
+    await message.answer("Доступные команды для вики:\n/" + "\n/".join(config.COMMANDS))
+
+
+@dp.message_handler(commands=config.COMMANDS)
+async def commands_wiki(message: types.Message):
+    """Возвращает ссылку на википедию по команде"""
+    await message.answer(message_filter.get_link_wiki(message.text))
+
+
 @dp.message_handler()
 async def start(message: types.Message):
-    await message.answer(message_filter.start_message(message))
+    """Приветствие"""
+    if message.text and not message.text.startswith("/"):
+        await message.reply(message_filter.start_message(message))
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
